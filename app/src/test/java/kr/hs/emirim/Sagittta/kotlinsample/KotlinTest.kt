08442534a8edf41f0097db2ebb6c55bd691bef22
Sagittta <js1verj@gmail.com>
2019-06-25 19:58:14 +0900

    My First Test Code

--- app/src/test/java/kr/hs/emirim/Sagittta/kotlinsample/KotlinTest.kt
+++ app/src/test/java/kr/hs/emirim/Sagittta/kotlinsample/KotlinTest.kt
@@ -1,2 +1,11 @@
 package kr.hs.emirim.Sagittta.kotlinsample
 
+import org.junit.Assert
+import org.junit.Test
+
+class KotlinTest {
+    @Test
+    fun test1() {
+        Assert.assertEquals(4, 1+3)
+    }
+}
